1e96c0ed4393ec9dc5278ea8829d04c1651805e8
jadejashrutiba279 <jadejashrutiba279@gmail.com>
2026-09-22 11:14:25 +0530

    Add files via upload

---
+++ exercise/npe.py
@@ -0,0 +1,18 @@
+import numpy
+a=numpy.array([10,20,30,40,50])
+for i in range(5):
+    print(a)
+
+
+import numpy as np
+b=np.array([10.5,20.5,30.5,40.5,50.5])
+for i in range(5):
+    print(b)
+
+
+from numpy import *
+c = array(['a', 'b', 'c', 'd', 'e'])
+for i in range(5):
+    print(c[i])
+
+
